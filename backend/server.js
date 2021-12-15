78ab73d6adda97247c776a79e4f522468f640e28
Necip <ncptzgn@gmail.com>
2021-12-15 16:44:14 +0300

    login user

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const dotenv= require("dotenv")
 const userRoute = require("./Routes/userRoute");
-const connectDB = require("./config/db");
+const connectDB = require("./Config/db");
 
 const app = express();
 app.use(express.json())
