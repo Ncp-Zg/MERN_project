eedd165f71ebe268e2180a499cc05aa3be1ffa3f
Necip <ncptzgn@gmail.com>
2022-01-29 17:04:57 +0300

    some styling in addProduct Page

--- frontend/src/components/AddProduct/Step1.tsx
+++ frontend/src/components/AddProduct/Step1.tsx
@@ -5,10 +5,10 @@ import React, {
   Dispatch,
   FunctionComponent,
   SetStateAction,
-  useEffect,
   useRef,
   useState,
 } from "react";
+import "./Step1.css";
 
 export interface form {
   formdata: {
@@ -59,7 +59,7 @@ const Step1: FunctionComponent<form> = (props) => {
         .then((data) => {
           setDelete_token([...delete_token, data.delete_token]);
           setLoading(false);
-          setFormData({...formdata,img:[...formdata.img,data.url]})
+          setFormData({ ...formdata, img: [...formdata.img, data.url] });
         })
         .catch((err) => {
           console.log(err);
@@ -68,20 +68,23 @@ const Step1: FunctionComponent<form> = (props) => {
   };
 
   const deleteCloud = async (i: number) => {
-    setDelLoading(true)
-    console.log(delete_token,i);
+    setDelLoading(true);
+    console.log(delete_token, i);
     await axios
       .post("https://api.cloudinary.com/v1_1/dl0na75ef/delete_by_token", {
         token: `${delete_token[i]}`,
       })
-      .then((res) => {console.log(res);
-        delete_token.splice(i,1);
-        formdata.img.splice(i,1);
-        setDelete_token(delete_token)
-        console.log(formdata.img)
-        setFormData({...formdata,img:formdata.img})
+      .then((res) => {
+        console.log(res);
+        delete_token.splice(i, 1);
+        formdata.img.splice(i, 1);
+        setDelete_token(delete_token);
+        console.log(formdata.img);
+        setFormData({ ...formdata, img: formdata.img });
 
-      setDelLoading(false)}).catch(()=>setDelLoading(false))
+        setDelLoading(false);
+      })
+      .catch(() => setDelLoading(false));
   };
 
   const categories = [
@@ -123,33 +126,47 @@ const Step1: FunctionComponent<form> = (props) => {
           borderRadius: "10px",
         }}
       >
-        {!delLoading ? formdata.img.length !== 0 ? (
-          formdata.img?.map((pre: any, index: number) => {
-            return (
-              <img
-                key={index}
-                src={pre}
-                alt=""
-                style={{
-                  objectFit: "cover",
-                  width: "100px",
-                  borderRadius: "10px",
-                  marginBottom: "10px",
-                  marginTop: "10px",
-                }}
-                onClick={() => {
-                  console.log(index);
-                  image.splice(index, 1);
-                  setImage([...image]);
-
-                  deleteCloud(index);
-                }}
-              />
-            );
-          })
+        {!delLoading ? (
+          formdata.img.length !== 0 ? (
+            formdata.img?.map((pre: any, index: number) => {
+              return (
+                <div className="imageBox">
+                  <img
+                    className="image"
+                    key={index}
+                    src={pre}
+                    alt=""
+                    style={{
+                      objectFit: "cover",
+                      width: "100px",
+                      height: "75px",
+                      borderRadius: "10px",
+                      marginBottom: "10px",
+                      marginTop: "10px",
+                      gridRowStart: "1",
+                      gridColumnStart: "1",
+                    }}
+                  />
+                  <img
+                    className="hoverImg"
+                    onClick={() => {
+                      console.log(index);
+                      image.splice(index, 1);
+                      setImage([...image]);
+                      deleteCloud(index);
+                    }}
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8xtyVAQplmfIf8EDW6K42xgGzLtfl84xT4A&usqp=CAU"
+                    alt="delete"
+                  />
+                </div>
+              );
+            })
+          ) : (
+            <h3>Add Picture</h3>
+          )
         ) : (
-          <h3>Add Picture</h3>
-        ) : <h3>Deleting..</h3>}
+          <h3>Deleting..</h3>
+        )}
       </div>
       <IconButton
         disabled={loading}
@@ -169,11 +186,10 @@ const Step1: FunctionComponent<form> = (props) => {
         accept="image/*"
         onChange={(event: any) => {
           const file = event.target.files[0];
-          
+
           if (file.size < 200000 && image.length < 5) {
             setImage([...image, file]);
             uploadPic(file);
-            
           } else if (file.size > 200000) {
             alert("please be sure your pic less than 200kb");
           } else if (image.length >= 5) {
