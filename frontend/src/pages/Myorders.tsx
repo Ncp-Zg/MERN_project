58b8b8438fd754b51329243995c68169dffc7b76
Necip <ncptzgn@gmail.com>
2022-04-28 16:25:05 +0300

    implementing react-window for myorders page

--- frontend/src/pages/Myorders.tsx
+++ frontend/src/pages/Myorders.tsx
@@ -1,77 +1,152 @@
-import { Button, Card } from "@mui/material"
-import axios from "axios"
-import moment from "moment"
-import { useEffect, useRef, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useNavigate } from "react-router-dom"
-import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader"
-import { setOrders } from "../redux/ActionCreators/OrdersActionCreators"
-import { IRootState } from "../redux/Reducers/rootReducer"
-import { Order } from "../type"
+import { Button, Card } from "@mui/material";
+import axios from "axios";
+import moment from "moment";
+import { CSSProperties, useCallback, useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import ClimbingBoxLoader from "react-spinners/ClimbingBoxLoader";
+import { FixedSizeList } from "react-window";
+import { setOrders } from "../redux/ActionCreators/OrdersActionCreators";
+import { IRootState } from "../redux/Reducers/rootReducer";
+import { Order } from "../type";
 
 const Myorders = () => {
-    const {user}=useSelector((state:IRootState)=>({user:state.auth}))
-    const [myOrders,setMyOrders] = useState<Order[]>([])
-    const [error, setError] = useState<boolean>(false)
-    const [loading, setLoading] = useState<boolean>(false)
-    const ref = useRef<number>(0)
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
+  const { user } = useSelector((state: IRootState) => ({ user: state.auth }));
+  const [myOrders, setMyOrders] = useState<Order[]>([]);
+  const [error, setError] = useState<boolean>(false);
+  const [loading, setLoading] = useState<boolean>(false);
+  const ref = useRef<number>(0);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    
-    const getMyOrders = async () => {
-        setLoading(true)
-        await axios.get("http://localhost:5000/api/orders/myorders",
-        {withCredentials:true}).then(res=>{setMyOrders(res.data.myorders);setLoading(false);dispatch(setOrders(res.data.myorders))}).catch(err=> err ? setError(true) : null)
+  const getMyOrders = async () => {
+    setLoading(true);
+    await axios
+      .get("http://localhost:5000/api/orders/myorders", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setMyOrders(res.data.myorders);
+        setLoading(false);
+        dispatch(setOrders(res.data.myorders));
+      })
+      .catch((err) => (err ? setError(true) : null));
+  };
+
+  useEffect(() => {
+    if (user.user.id) {
+      getMyOrders();
     }
+  }, [user?.user.id]);
 
+  const reverseArr = [...myOrders].reverse()
 
-    useEffect(()=>{
-        if(user.user.id){
-            getMyOrders();
-        }
-        
-    },[user?.user.id])
+  const Row = useCallback(
+    ({ index, style }: { index: number; style: CSSProperties }) => {
+      return (
+        <div style={style}>
+        <Card
+          sx={{
+            display: "grid",
+            gridAutoRows: "1fr,1fr,1fr",
+            width: "80vh",
+            padding: "5px 5px 5px 5px",
+            alignItems: "center",
+            marginBottom: "5px",
+          }}
+        >
+          <h3>{moment(reverseArr[index].createdAt).format("LLL")}</h3>
 
-
-    return (
-        <div>
+          <h3>
+            {reverseArr[index].order.length} brand{" "}
             {
-                myOrders.length !== 0 ? (
-                    [...myOrders].reverse()?.map(order=>(
-                        <Card
-                key={order._id}
-                sx={{
-                  display: "grid",
-                  gridAutoRows:"1fr,1fr,1fr",
-                  width:"80vh",
-                    padding:"5px 5px 5px 5px",
-                  alignItems: "center",
-                  marginBottom: "5px",
-                }}
-              >
-                  <h3>{moment(order.createdAt).format("LLL")}</h3>
-                
-                <h3>
-                    {order.order.length} brand {" "}
-                    {ref.current = order.order.map((c,index) => +order.amount[index] * +c.cost)
-                    .reduce((prev, curr) => prev + curr, 0)}₺
-                </h3>
-                <div style={{display:"flex",justifyContent:"end",alignItems:"center"}}>
-                  <Button variant="contained" size="small" onClick={()=>navigate(`/profile/myorders/${order._id}`,{state:order})}>Details</Button>  
-                </div>
-                
-                    
-      
-              </Card>
-                    ))
-                ) : error ? 
-                (
-                    <h3>need authorization</h3>
-                )
-                :
-                loading ? (
-                    <div
+              (ref.current = reverseArr[index].order
+                .map((c, index) => +reverseArr[index].amount[index] * +c.cost)
+                .reduce((prev, curr) => prev + curr, 0))
+            }
+            ₺
+          </h3>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "end",
+              alignItems: "center",
+            }}
+          >
+            <Button
+              variant="contained"
+              size="small"
+              onClick={() =>
+                navigate(`/profile/myorders/${reverseArr[index]._id}`)
+              }
+            >
+              Details
+            </Button>
+          </div>
+        </Card>
+        </div>
+      );
+    },
+    [reverseArr]
+  );
+
+  return (
+    <div>
+      {myOrders.length !== 0 ? (
+        <FixedSizeList
+        height={550}
+        width={800}
+        itemSize={205}
+        itemCount={myOrders.length}
+      >
+        {Row}
+      </FixedSizeList>
+        // [...myOrders].reverse()?.map((order) => (
+        //   <Card
+        //     key={order._id}
+        //     sx={{
+        //       display: "grid",
+        //       gridAutoRows: "1fr,1fr,1fr",
+        //       width: "80vh",
+        //       padding: "5px 5px 5px 5px",
+        //       alignItems: "center",
+        //       marginBottom: "5px",
+        //     }}
+        //   >
+        //     <h3>{moment(order.createdAt).format("LLL")}</h3>
+
+        //     <h3>
+        //       {order.order.length} brand{" "}
+        //       {
+        //         (ref.current = order.order
+        //           .map((c, index) => +order.amount[index] * +c.cost)
+        //           .reduce((prev, curr) => prev + curr, 0))
+        //       }
+        //       ₺
+        //     </h3>
+        //     <div
+        //       style={{
+        //         display: "flex",
+        //         justifyContent: "end",
+        //         alignItems: "center",
+        //       }}
+        //     >
+        //       <Button
+        //         variant="contained"
+        //         size="small"
+        //         onClick={() =>
+        //           navigate(`/profile/myorders/${order._id}`, { state: order })
+        //         }
+        //       >
+        //         Details
+        //       </Button>
+        //     </div>
+        //   </Card>
+        // ))
+      ) : error ? (
+        <h3>need authorization</h3>
+      ) : loading ? (
+        <div
           style={{
             display: "flex",
             justifyContent: "center",
@@ -81,16 +156,11 @@ const Myorders = () => {
         >
           <ClimbingBoxLoader size={30} color="#c67c03" />
         </div>
-                )
-                :
-                (
-                    <h3>
-                        no order
-                    </h3>
-                )
-            }
-        </div>
-    )
-}
+      ) : (
+        <h3>no order</h3>
+      )}
+    </div>
+  );
+};
 
-export default Myorders
+export default Myorders;
